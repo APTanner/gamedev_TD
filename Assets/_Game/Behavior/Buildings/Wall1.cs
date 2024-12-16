cca68db72ab00baebcfa53f0f1a922044e08ff67
BuildTools <jimmyadkins@me.com>
2024-12-16 13:13:03 -0600

    Added small changes to scenes

---
+++ Assets/_Game/Behavior/Buildings/Wall1.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Wall1 : BuildableObject
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        Health = 1000;
+        Price = 25;
+    }
+
+    public override Vector2Int Size => new Vector2Int(1, 1);
+
+}
